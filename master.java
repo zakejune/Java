80035776e1d976abb03d9cc6b15e3d2f9d111a01
zakejune <36754366+zakejune@users.noreply.github.com>
2019-10-20 23:52:33 +0800

    Create master.java

---
+++ master.java
@@ -0,0 +1,16 @@
+ /**
+ *  @author xiaozhu
+ * private封装
+ */
+
+class Person {
+    private String name;
+    private int age;
+
+    public void tell(){
+    }
+
+    public void setName(String tempName){
+    name = tempName;
+    }
+}
