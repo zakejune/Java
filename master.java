b6e6793c497bc645eafcc4e70e657790129b4885
zakejune <juneyoung1204@outlook.com>
2019-10-23 19:40:44 +0800

    Update master.java

--- master.java
+++ master.java
@@ -8,9 +8,32 @@ class Person {
     private int age;
 
     public void tell(){
+        System.out.println("name: "+name);
     }
+
 
     public void setName(String tempName){
     name = tempName;
     }
+
+    public void setAge(int tempAge) {
+        if(tempAge > 0 && tempAge <200) {
+            age = tempAge;
+        }
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
+public class master {
+    public static void main(String args[]) {
+        Person per = new Person();
+        per.setName("xiaozhu");
+        per.setAge(20);
+    }
+}
+    
