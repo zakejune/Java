d5275c5abddfe23a2d9f92e8189570a7d818846d
zakejune <juneyoung1204@outlook.com>
2019-10-30 23:37:18 +0800

    java的数组基本定义

---
+++ arr.java
@@ -0,0 +1,16 @@
+/**
+ * @author xiaozhu
+ * date 2019/10/30
+ * 尽量每天都写
+ * java数组
+ */
+public class arr {
+    public static void main(String[] args) {
+        int data [] =new int [3]; //动态初始化  
+        data [0] = 1;
+        data [1] = 3;
+        for ( int a=0;a<data.length;a++) {
+            System.out.println(data[a]  );
+        }
+    }
+}
