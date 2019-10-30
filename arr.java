25672afd3c3dadd5f332085816588bce78f2a434
zakejune <juneyoung1204@outlook.com>
2019-10-30 23:47:04 +0800

    Update arr.java

--- arr.java
+++ arr.java
@@ -10,13 +10,23 @@ public class arr {
         data [0] = 1;
         data [1] = 3;
         int dic[] = new int[] {12,35,65};
-
+        int tp[] = dic;
+        tp[2] = 17;
+        System.out.println("dic[3] is");
         for ( int a=0;a<dic.length;a++) {
             System.out.println(dic[a]  );
         }
+
         System.out.println("#########");
+        System.out.println("data[3] is ");
         for ( int a=0;a<data.length;a++) {
             System.out.println(data[a]  );
         }
+        System.out.println("#########");
+        System.out.println("tp[3] is ");
+        for ( int a=0;a<tp.length;a++) {
+            System.out.println(tp[a]  );
+        }
+        
     }
 }
